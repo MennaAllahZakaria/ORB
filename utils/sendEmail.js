eb5d85f87171e8db8ecfe7a991e0d5d51a7a5680
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2026-04-02 17:23:36 +0200

    Refactor email transporter configuration

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,13 +2,12 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    connectionTimeout: 10000,
-  });
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
   const mailOpts = {
     from: "ORB <orb.company.edu@gmail.com>",
