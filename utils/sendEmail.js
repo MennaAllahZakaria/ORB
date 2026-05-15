bf06849fd9f913e2507743abf989782845f16519
MennaAllahZakaria <mennazakaria2003@gmail.com>
2026-05-16 01:19:32 +0300

    refactor: replace Google API email sending with Nodemailer for improved simplicity and reliability

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,44 +1,26 @@
-const { google } = require("googleapis");
+const nodemailer = require("nodemailer");
 
-const sendEmail = async ({ Email, subject, message }) => {
-  if (!Email || typeof Email !== "string") {
-    throw new Error("Invalid email address");
-  }
-
-  if (!subject || !message) {
-    throw new Error("Missing subject or message");
-  }
-
-  const oAuth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
-
-  oAuth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-  });
-
-  const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
-
-  const rawMessage = Buffer.from(
-    `From: ORB <${process.env.EMAIL_USER}>
-To: ${Email}
-Subject: ${subject}
-
-${message}`
-  )
-    .toString("base64")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=+$/, "");
-
-  await gmail.users.messages.send({
-    userId: "me",
-    requestBody: {
-      raw: rawMessage,
+// Nodemailer
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT, // if secure false port = 587, if true port= 465
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER, 
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
+
+  const mailOpts = {
+    from: `ORB <${process.env.EMAIL_USER}>`,
+    to: options.Email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  // 3) Send email
+  await transporter.sendMail(mailOpts);
 };
 
 module.exports = sendEmail;
