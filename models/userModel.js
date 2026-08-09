065401d69cf762990f25822fc7aaf4e69fa8a6d1
MennaAllahZakaria <mennazakaria2003@gmail.com>
2026-08-09 04:24:34 +0300

    feat: update user schema to conditionally require profiles based on profile completion status

--- models/userModel.js
+++ models/userModel.js
@@ -151,14 +151,14 @@ const userSchema = new mongoose.Schema(
     teacherProfile: {
       type: teacherProfileSchema,
       required: function () {
-        return this.role === "teacher" && !this.googleId ;
+        return this.role === "teacher" && !this.isProfileCompleted ;
       },
     },
 
     studentProfile: {
       type: studentProfileSchema,
       required: function () {
-        return this.role === "student" &&!this.googleId ;
+        return this.role === "student" &&!this.isProfileCompleted ;
       },
     },
 
